ae9db5d3c686bfeddd3e7061f54fbb94fb942360
dilaramuhtar <dilaramuhtaruse@gmail.com>
2026-03-22 12:50:27 -0400

    Initial commit: added project structure and Main.java

--- src/day01_commit_pushi_pull/Main.java
+++ src/day01_commit_pushi_pull/Main.java
@@ -4,6 +4,6 @@ package day01_commit_pushi_pull;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello，Git");
+        System.out.println("Hello，  Git");
     }
 }
